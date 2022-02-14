aa22d7b2a7a61f25f69ac14cd3e775feddc8ed7e
이성현 <marvel97@naver.com>
2022-02-14 11:34:11 +0900

    zombiehelper.sol

--- contracts/zombiehelper.sol
+++ contracts/zombiehelper.sol
@@ -1,25 +1,36 @@
 pragma solidity ^0.4.19;
+
 import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
+    uint256 levelUpFee = 0.001 ether;
+
     modifier aboveLevel(uint256 _level, uint256 _zombieId) {
         require(zombies[_zombieId].level >= _level);
         _;
     }
 
+    function withdraw() external onlyOwner {
+        owner.transfer(this.balance);
+    }
+
+    function setLevelUpFee(uint256 _fee) external onlyOwner {
+        levelUpFee = _fee;
+    }
+
     function changeName(uint256 _zombieId, string _newName)
         external
         aboveLevel(2, _zombieId)
+        ownerOf(_zombieId)
     {
-        require(msg.sender == zombieToOwner[_zombieId]);
         zombies[_zombieId].name = _newName;
     }
 
     function changeDna(uint256 _zombieId, uint256 _newDna)
         external
         aboveLevel(20, _zombieId)
+        ownerOf(_zombieId)
     {
-        require(msg.sender == zombieToOwner[_zombieId]);
         zombies[_zombieId].dna = _newDna;
     }
 
