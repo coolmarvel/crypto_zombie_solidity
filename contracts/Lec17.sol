de6b48271e099d6fa8921bbdfd81c68d0f9111b1
이성현 <marvel97@naver.com>
2022-11-03 17:02:06 +0900

    mapping

--- contracts/Lec17.sol
+++ contracts/Lec17.sol
@@ -5,6 +5,7 @@ contract Lec17 {
     mapping(string => uint256) private priceList;
     mapping(uint256 => string) private nameList;
     mapping(uint256 => uint256) private ageList;
+    // mapping에는 length 내장함수가 없기에 length를 구할 수 가 없다
 
     function setAgeList(uint256 _key, uint256 _age) public {
         ageList[_key] = _age;
